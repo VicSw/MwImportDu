880bd2ab950965e5688dfcf79d76ab9313fe0012
vicSun <sunwei1818@163.com>
2019-05-19 23:28:28 +0800

    add
    add

---
+++ src/routes.js
@@ -0,0 +1,10 @@
+import Login from './views/Login.vue'
+
+let routes = [{
+    path: '/Login',
+    component: Login,
+    name: '',
+    hidden: true
+}];
+
+export default routes;
---
+++ src/views/Login.vue
@@ -0,0 +1,91 @@
+<template>
+  <el-form :model="ruleForm2" :rules="rules2" ref="ruleForm2" label-position="left" label-width="0px" class="demo-ruleForm login-container">
+    <h3 class="title">换单平台登录</h3>
+    <el-form-item prop="account">
+      <el-input type="text" v-model="ruleForm2.account" auto-complete="off" placeholder="账号"></el-input>
+    </el-form-item>
+    <el-form-item prop="checkPass">
+      <el-input type="password" v-model="ruleForm2.checkPass" auto-complete="off" placeholder="密码"></el-input>
+    </el-form-item>
+    <el-link type="primary" class="reg">注册</el-link>|
+    <el-checkbox v-model="checked" checked class="remember">记住密码</el-checkbox>
+    <el-form-item style="width:100%;">
+      <el-button type="success" style="width:100%;" @click.native.prevent="handleSubmit2" :loading="logining">登录</el-button>
+      <!--<el-button @click.native.prevent="handleReset2">重置</el-button>-->
+    </el-form-item>
+  </el-form>
+</template>
+
+<script>
+
+  export default {
+    data() {
+      return {
+        logining: false,
+        ruleForm2: {
+          account: 'admin',
+          checkPass: '12345'
+        },
+        rules2: {
+          account: [
+            { required: true, message: '请输入账号', trigger: 'blur' },
+            //{ validator: validaePass }
+          ],
+          checkPass: [
+            { required: true, message: '请输入密码', trigger: 'blur' },
+            //{ validator: validaePass2 }
+          ]
+        },
+        checked: true
+      };
+    },
+    methods: {
+      handleReset2() {
+        this.$refs.ruleForm2.resetFields();
+      },
+      handleSubmit2(ev) {
+        var _this = this;
+        this.$refs.ruleForm2.validate((valid) => {
+              //调用登陆API
+              if(this.ruleForm2.account!=='admin'){
+                  alert("用户名不存在！");
+              }
+              else if(this.ruleForm2.checkPass!=='12345'){
+                alert("密码不正确!");
+              }
+              else{
+                alert("登陆成功！");
+              }
+        });
+      }
+    }
+  }
+</script>
+<style lang="scss" scoped>
+@media screen and (min-device-width : 375px)
+{
+  .login-container {
+    -webkit-border-radius: 5px;
+    border-radius: 5px;
+    -moz-border-radius: 5px;
+    background-clip: padding-box;
+    margin: 180px auto;
+    width: 350px;
+    padding: 35px 35px 15px 35px;
+    background: #fff;
+    border: 1px solid #eaeaea;
+    box-shadow: 0 0 25px #cac6c6;
+    .title {
+      margin:0px auto 40px auto;
+      text-align:center;
+      color: #505458;
+    }
+    .remember {
+      margin: 0px 0px 35px 20px;
+    }
+    .reg{
+      margin: 0px 20px 0px 0px;
+    }
+  }
+}
+</style>
